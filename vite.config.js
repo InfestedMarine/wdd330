22f1eb6dabdbf746556216f0bfaf8f3eddbc98e4
InfestedMarine <cyandsanchez@gmail.com>
2025-09-29 12:31:15 +0800

    wILL this fix it?!

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     outDir: '../dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, '/src/index.html'),
-        cart: resolve(__dirname, '/src/cart/index.html'),
-        checkout: resolve(__dirname, '/src/checkout/index.html'),
-        productListing: resolve(__dirname, '/src/product_listing/index.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        cart: resolve(__dirname, 'src/cart/index.html'),
+        checkout: resolve(__dirname, 'src/checkout/index.html'),
+        productListing: resolve(__dirname, 'src/product_listing/index.html'),
         product1: resolve(
           __dirname,
           'src/product_pages/cedar-ridge-rimrock-2.html',
